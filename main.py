637df1c30f99798944efdecdd25f17eb3a537800
JRBros2346 <jrbros2346@gmail.com>
2023-04-28 11:08:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import Terminal
 
 screen=Terminal.Screen(60,25)
-screen.rect(6,5,1,1,0,0,255)
-screen.rect(6,6,1,1,255,255,0)
+#screen.rect(6,5,1,1,0,0,255)
+#screen.rect(6,6,1,1,255,255,0)
+screen.pixels[5][6]=[0,255,0]
+screen.pixels[6][6]=[255,0,255]
 screen.display()
