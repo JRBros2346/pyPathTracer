e9f485b8593f658ef3dd8386e4ba585ce7feb924
JRBros2346 <jrbros2346@gmail.com>
2023-09-24 20:04:38 +0530

    start

--- Terminal.py
+++ Terminal.py
@@ -1,13 +1,29 @@
+#class Screen:
+#    color=lambda up,down:'\033[48;2;{};{};{}m\033[38;2;{};{};{}m\u2584\033[0m'.format(up[0],up[1],up[2],down[0],down[1],down[2])
+#    def __init__(self,width,height):
+#        self.width=width
+#        self.height=height
+#        self.pixels=[[[0]*3]*width]*2*height
+#    def display(self):
+#        for h in range(self.height):
+#            for w in range(self.width):
+#                print(Screen.color(self.pixels[2*h][w],self.pixels[2*h+1][w]),end='')
+#            print()
+#    def rect(self,x,y,w,h,r,g,b):
+#        for i in range(y,y+h):
+#            for j in range(x,x+w):
+#                self.pixels[i][j]=[r,g,b]
+
 class Screen:
-    color=lambda up,down:'\033[48;2;{};{};{}m\033[38;2;{};{};{}m\u2584\033[0m'.format(up[0],up[1],up[2],down[0],down[1],down[2])
+    color=lambda r,g,b:'\033[48;2;{};{};{}m \033[0m'.format(r,g,b)
     def __init__(self,width,height):
         self.width=width
         self.height=height
-        self.pixels=[[[0]*3]*width]*2*height
+        self.pixels=[[[0]*3]*width]*height
     def display(self):
         for h in range(self.height):
             for w in range(self.width):
-                print(Screen.color(self.pixels[2*h][w],self.pixels[2*h+1][w]),end='')
+                print(Screen.color(*self.pixels[h][w]))
             print()
     def rect(self,x,y,w,h,r,g,b):
         for i in range(y,y+h):
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+WIDTH=60
+HEIGHT=25
+
 import Terminal
 
-screen=Terminal.Screen(60,25)
-#screen.rect(6,5,1,1,0,0,255)
-#screen.rect(6,6,1,1,255,255,0)
-screen.pixels[5][6]=[0,255,0]
-screen.pixels[6][6]=[255,0,255]
+screen=Terminal.Screen(WIDTH,HEIGHT)
+screen.rect(6,5,1,1,0,0,255)
+screen.rect(6,6,1,1,255,255,0)
+#screen.pixels[5][6]=[0,255,0]
+#screen.pixels[6][6]=[255,0,255]
 screen.display()
